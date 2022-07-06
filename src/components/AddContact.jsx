fb54f064a3bec86e221785ca58762a90981c881b
alialaba <alialaba079@gmail.com>
2022-07-06 15:40:09 -0400

    style: add bg,color, margin, padding to contact list ui

--- src/components/AddContact.jsx
+++ src/components/AddContact.jsx
@@ -3,15 +3,11 @@ export default function AddContact(){
     return(
         <form className="add-form">
             <div className="form-control">
-                <label>First name</label>
+                <label>Name</label>
                 <input type="text"/>
 
             </div>
-            <div className="form-control">
-                <label>Surname</label>
-                <input type="text"/>
-
-            </div>
+            
             <div className="form-control">
                 <label>Email</label>
                 <input type="text"/>
